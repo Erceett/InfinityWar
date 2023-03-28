f03c3af4e027f0fd5c5f7fb0606d6cf5290fb813
Erceett <omerfercetin@gmail.com>
2023-03-28 15:36:25 +0300

    28.3.23
    added some player movements

--- iw/Assets/Scripts/PlayerControls.cs
+++ iw/Assets/Scripts/PlayerControls.cs
@@ -1,24 +1,46 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.NetworkInformation;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerControls : MonoBehaviour
 {
     [SerializeField] float speed = 1f;
+    [SerializeField] float xRange = 5f;
+    [SerializeField] float yRange = 5f;
+    [SerializeField] float positionPitchFactor = -2f;
 
     void Update()
     {
+        ProcessTranslation();
+        ProcessRotation();
+    }
+
+    private void ProcessRotation()
+    {
+        float pitch = transform.localPosition.y * positionPitchFactor;
+        float yaw = transform.localPosition.x * positionPitchFactor;
+        float zRotation = 0f;
+        
+
+        transform.localRotation = Quaternion.Euler(pitch, yaw, zRotation);
+    }
+
+    private void ProcessTranslation()
+    {
         float xThrow = Input.GetAxis("Horizontal");
-        Debug.Log("horizontal = " + " " + xThrow);
+        float xOffset = xThrow * Time.deltaTime * speed;
+        float newXPos = transform.localPosition.x + xOffset;
+        float clampedXPoss = Mathf.Clamp(newXPos, -xRange, xRange);
 
         float yThrow = Input.GetAxis("Vertical");
-        Debug.Log("vertical = " + " " + yThrow);
+        float yOffset = yThrow * Time.deltaTime * speed;
+        float newYPos = transform.localPosition.y + yOffset;
+        float clampedYPoss = Mathf.Clamp(newYPos, -yRange, yRange);
 
-        float xValue = xThrow * Time.deltaTime * speed;
-        float yValue = yThrow * Time.deltaTime * speed;
-
-        transform.Translate(xValue, yValue , 0);
+        transform.localPosition = new Vector3(clampedXPoss, clampedYPoss, transform.localPosition.z);
     }
 }
